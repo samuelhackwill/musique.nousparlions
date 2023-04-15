dab9491b5339ea4c5a43bbc178e6c75b120a514c
samuelhackwill <hackmew@gmail.com>
2023-04-15 20:02:26 +0200

    Change chatbot markup to scope css

--- imports/ui/pages/chatbot.js
+++ imports/ui/pages/chatbot.js
@@ -6,15 +6,15 @@ import {data} from '../../API/text/chatbot.js';
 Template.chatbotSVG.onRendered(function(){
     document.body.style.backgroundColor = "black"
 
-    document.getElementById("bg").addEventListener("animationend", function(){
+    document.getElementById("chatbot-bg").addEventListener("animationend", function(){
         document.body.style.transition = "all 1s"
         // when title fades out, we want to first show piano, then the rest.
         console.log("title faded out, let's go bg")
         document.body.style.backgroundColor = "rgb(255,60,0)"
 
-        document.getElementById("bg").style.opacity = 1
-        document.getElementById("sam").style.opacity = 1
-        document.getElementById("stef").style.opacity = 1
+        document.getElementById("chatbot-bg").style.opacity = 1
+        document.getElementById("chatbot-sam").style.opacity = 1
+        document.getElementById("chatbot-stef").style.opacity = 1
         document.getElementById("maes").style.opacity = 1
         // document.addEventListener("keyup", next)
         // document.addEventListener("touchstart", touchtouch)
